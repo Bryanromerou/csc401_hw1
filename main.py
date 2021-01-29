fcea56fb4aecca7fab281fc1023fb5876f2c01ec
Bryan Romero <bryanromerou@gmail.com>
2021-01-29 14:36:10 -0800

    Completed an inefficent way to populate and shuffle the males and females

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 import sys
+import random
 
-no_men = sys.argv[1]
-print(f'The number of men are {no_men}')
+def match_maker(men,women):
+    print("This is working")
+    matched_women = {}
+    available_men = []
+    for man in men:
+        available_men.append(man)
+    while(available_men):
+        print("working")
+        print(available_men.pop())
+
+def populate(n):
+    men = {}
+    women = {}
+
+    for i in range(0,n):
+        men[f'M{i}'] = []
+        women[f'F{i}'] = []
+
+        for j in range(0,n):
+            men[f'M{i}'].append(f'F{j}')
+            women[f'F{i}'].append(f'M{j}')
+
+        random.shuffle(men[f'M{i}'])
+        random.shuffle(women[f'F{i}'])
+    
+    for i, man in enumerate(men):
+        print(man, end = " ")
+        print(men[f"M{i}"])
+    
+    print(" ")
+
+    for i, woman in enumerate(women):
+        print(woman, end = " ")
+        print(women[f"F{i}"])
+    
+    match_maker(men,women)
+    return [men,women]
+
+n = int(sys.argv[1])
+populate(n)
